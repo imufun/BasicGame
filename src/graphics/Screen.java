c5f11f9f4eed2392a399b6d6b2faa5c9b5fc3c2e
Imran <imufun1@gmail.com>
2016-05-21 16:14:33 +0600

    update

--- src/graphics/Screen.java
+++ src/graphics/Screen.java
@@ -6,7 +6,9 @@ public class Screen {
 	private int width, height;
 	public int[] pixles;
 
-	int time = 0;
+	int xtime = 100;
+	int ytime = 50;
+
 	int counter = 50;
 
 	public Screen(int width, int height) {
@@ -17,20 +19,24 @@ public class Screen {
 
 	public void clear() {
 		for (int i = 0; i < pixles.length; i++) {
-			pixles[i]=0;
-			//im
+			pixles[i] = 0;
+			// im
 		}
 	}
 
 	public void render() {
 		counter++;
-		if (counter % 100 == 0) {
-			time++;
-		}
+		if (counter % 100 == 0)
+			xtime--;
+		if (counter % 100 == 0)
+			ytime--;
 
 		for (int y = 0; y < height; y++) {
+			if (ytime <0 || ytime >= height)
+				break;
 			for (int x = 0; x < width; x++) {
-				pixles[time + time * width] = 0xff00ff;
+				if (xtime < 0 || xtime >= width)break;
+				pixles[xtime + ytime * width] = 0xff00ff;
 			}
 		}
 	}
